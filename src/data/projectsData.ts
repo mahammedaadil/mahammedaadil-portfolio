067e4fcc77ea959531321eb9c499ec99cb9cdc26
mahammedaadil patel <adilchoice30@gmail.com>
2025-11-24 17:40:36 +0530

    added project update

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -10,5 +10,16 @@ export const projects: Project[] = [
     live: 'https://aadicare-healthcare.netlify.app/',
     image: 'https://www.startlazaa.com/wp-content/uploads/hospital-management-software-hospital-management-information-software-startlazaa-1200x600.png'
  
+  },
+
+  {
+    id: 2,
+    title: 'Personal Portfolio',
+    description: 'Porfolio Which Include Infromation Of My Resume',
+    technologies: ['React', 'Node.js', 'Express', 'MongoDB',],
+    github: 'https://github.com/mahammedaadil/mahammedaadil-portfolio.git',
+    live: 'https://mahammedaadil-portfolio.netlify.app',
+    image: 'https://tint.creativemarket.com/lqU1IZwUw4HHPPwvET5xd6aCqNrJ8n4zYIqGhcuq8BY/width:1200/height:800/gravity:nowe/rt:fill-down/el:1/czM6Ly9maWxlcy5jcmVhdGl2ZW1hcmtldC5jb20vaW1hZ2VzL3NjcmVlbnNob3RzL3Byb2R1Y3RzLzU0MDUvNTQwNTkvNTQwNTk2NzgvdjJfZGVzaWducG9ydGZvbGlvLXRlbXBsYXRlLXR5cGVmb29sLXByb21vdGlvbmFsLW8uanBnIzE3NTQzOTI3MDc?1754392707'
+ 
   }
 ];
